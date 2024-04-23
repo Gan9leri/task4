5bdbd309c0bc90ca1e124f471056bc67514f8777
AOvsyannikov <aovsyannikov@it-one.ru>
2024-04-23 12:59:01 +0300

    added selenide_test.java

---
+++ src/test/java/tests/SelenideTest.java
@@ -0,0 +1,40 @@
+/*Разработайте следующий автотест:
+- Откройте страницу Selenide в Github
+- Перейдите в раздел Wiki проекта
+- Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+- Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+*/
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void selenide_test() {
+        //- Откройте страницу Selenide в Github
+        open("/selenide/selenide");
+        //- Перейдите в раздел Wiki проекта
+        $("#wiki-tab").click();
+        //- Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-body .markdown-body ul").shouldHave(text("Soft assertions"));
+        //- Откройте страницу SoftAssertions
+        $("#wiki-body .markdown-body ul").$(byTagAndText("a", "Soft assertions")).click();
+        //проверьте что внутри есть пример кода для JUnit5
+        $("#wiki-body .markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
